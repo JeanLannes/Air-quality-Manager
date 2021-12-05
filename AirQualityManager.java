b253773ef4c465e4e8dd7ab5080459843f6f8a36
JeanLannes <95310014+JeanLannes@users.noreply.github.com>
2021-12-05 05:01:36 +0100

    Version 0.1
    Le programme est rudimentaire
    - Les fichiers externes ont été ajoutés
    - Les données des fichiers peuvent désormais être importés via les méthodes importSensors() et importValues()

--- AirQualityManager.java
+++ AirQualityManager.java
@@ -1,6 +1,61 @@
+import java.io.*;
+import java.util.*;
+
 public class AirQualityManager{
 
-	public static void main(String[] args) {
+	public static ArrayList<String> importSensors() throws IOException
+	{
+
+	public static ArrayList<String> importValues() throws IOException
+	{
+		String line;
+		ArrayList<String> valeursData = new ArrayList<String>();
+		try {
+			FileReader valeursReader = new FileReader("valeurs.csv");
+			BufferedReader valeurs = new BufferedReader(valeursReader);
+			while((line=valeurs.readLine()) != null) {
+				valeursData.add(line);
+			}
+			valeursReader = new FileReader("valeurs.csv");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}		
+		return valeursData;
+	}
+
+	public class exportData{
+
 		
 	}
+
+	public static void main(String[] args) throws IOException {
+
+		int choice, cond=0;
+		Scanner readKeyboard = new Scanner(System.in);	
+		ArrayList<String> listOfSensors = new ArrayList<String>();
+		listOfSensors = importSensors();
+		ArrayList<String> listOfValues = new ArrayList<String>();
+		listOfValues = importValues();
+
+
+		do{
+			System.out.println("******** MENU ********\n  1- \n  2-  \n  3- \n  4- \n  5- \n  6- \n  10- Quitter\n**********************");
+			choice=readKeyboard.nextInt();	
+			switch(choice){
+				case 1: System.out.println(listOfSensors.get(1));
+						System.out.println(listOfSensors.get(2));
+						System.out.println(listOfValues.get(1));
+						System.out.println(listOfValues.get(2));
+						break;
+				case 2:
+						break;
+				case 10:System.out.println("\n * AU REVOIR *\n");
+						cond++;
+						break;
+				default:System.out.println("\n * VALEUR INCORRECTE *\n");
+			}
+		} while(cond==0);
+
+	}
 }
+
